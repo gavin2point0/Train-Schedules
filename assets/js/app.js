2213ec8b8b6fb08ae06dd8fce5cb9b8bb69b20c8
Gavin Peterson <gavinpeterson@Gavins-MacBook-Pro.local>
2018-04-27 09:57:58 -0500

    doing math for trains

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,6 +23,11 @@ $('.btn-primary').on('click', function(){
         firstTrain: firstTrain,
         frequency: trainFrequency
     })
+    
+    trainName.text('')
+    trainDestination.text('')
+    firstTrain.text('')
+    trainFrequency.text('')
 
 })
 
@@ -34,12 +39,44 @@ database.ref().on('child_added', function(snapshot){
     firstTrainCell = snapshot.val().firstTrain;
     frequencyCell = snapshot.val().frequency;
 
-    let newRowContent = $(`<td id="train-name">${nameCell}</td><td id="destination">${destinationCell}</td><td id="frequency">${frequencyCell}</td>`)
 
-    console.log(snapshot);
-    console.log(destinationCell);
-    console.log(firstTrainCell);
-    console.log(frequencyCell);
+    // let nextArrivalCell = moment(firstTrainCell.toString()).add(parseInt(frequencyCell), 'minutes');
+
+
+    // Assumptions
+    var tFrequency = 3;
+
+    // Time is 3:30 AM
+    var firstTime = "03:30";
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTrainCell, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequencyCell;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = frequencyCell - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+
+    
+    let newRowContent = $(`<td id="train-name">${nameCell}</td><td id="destination">${destinationCell}</td><td id="frequency">${frequencyCell}</td><td id="next-train">${moment(nextTrain).format("hh:mm")}</td><td id="minutes-away">${tMinutesTillTrain}</td>`)
 
     newRow.append(newRowContent);
     $('.table').append(newRow);
